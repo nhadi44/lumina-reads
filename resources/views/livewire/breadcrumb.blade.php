3f671e254c4b074092696dbc984a989438e7aca4
nhadi44 <hadinurhidayat97@hotmail.com>
2024-01-17 19:59:58 +0700

    fisrt commit

---
+++ resources/views/livewire/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+<nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.html">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+            Layout Vertical Navbar
+        </li>
+    </ol>
+</nav>
---
+++ resources/views/livewire/page-heading.blade.php
@@ -0,0 +1,3 @@
+<div class="page-heading">
+    <livewire:page-title/>
+</div>
